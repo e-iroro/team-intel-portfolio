85cf2e9c55aad59c92094265ba131242f222973c
nancy4hany <nancyhany8@gmail.com>
2023-01-13 17:06:54 +0200

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'avenir-light': ['"Avenir Light"', 'sans-serif'],
+      'questrial': ['"Questrial"', 'sans-serif']
+    },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      // transparent: 'transparent',
+      // current: 'currentColor',
       'pink': '#F2458E',
       'black': '#181818',
       'light-pink': '#FFEFED',     
       'blue': '#276EF1',     
       'violet': '#907AD6',     
       'white': '#ffffff',
+      
+      'purple-400': '#907AD6',
+      brightRed:'hsl(12,88%,59%)',
+      brightRedLight:'hsl(12,88%,69%)',
+      brightRedSupLight:'hsl(12,88%,96%)',
+      darkBlue:'hsl(228,39%,23%)',
+      darkGrayishBlue:'hsl(227,12%,61%)',
+      veryDarkBlue:'hsl(223,12%,13%)',
+      veryPaleRed: 'hsl(12,100%,96%)',
+      veryLightGray:'hsl(0,0%,98&)'
     },
     extend: {
       fontFamily: {
