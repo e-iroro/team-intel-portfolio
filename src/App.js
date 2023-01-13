b496e39e5310f26f8e9d4841117894a9aa97fcb6
e-iroro <eruiroro18@gmail.com>
2023-01-13 20:40:03 +0100

    add project page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import Test from './components/Test';
+import ProjectCard from './components/ProjectCard';
+import projects from './project';
+
+
+function createProjectCard(project) {
+  return (
+    <ProjectCard
+      key = {project.id}
+      alt = {project.alt}
+      image = {project.image}
+      name = {project.name}
+      description = {project.description}
+      category = {project.category}
+      project = {project.project}
+      projectLink={project.projectlink}
+      repoLink={project.repo}
+     />
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App ">
+    <div className=" w-3/4 mx-auto ">
         <Test />
+        {projects.map(createProjectCard)}
     </div>
   );
 }
---
+++ src/project.js
@@ -0,0 +1,43 @@
+const projects = [
+    {
+        id: 1,
+        image:'https://res.cloudinary.com/eiroro/image/upload/v1673533432/team-intel-portfolio/rh56syv4sxupymxngayu.png',
+        alt: "An image of the app screens",
+        name:"Amusan Portfolio",
+        description : "A portfolio page for a photographer",
+        category: "Web Development, HTML, CSS" ,
+        project:"https://www.jayma.design/redesigning-rental-pickups-uber",
+        repo:"https://github.com/blessdamy/Profile-project"    ,
+        projectlink: "https://profile-project1.netlify.app"
+    },
+    {
+        id: 2,
+        image:'https://res.cloudinary.com/eiroro/image/upload/v1673533750/team-intel-portfolio/qtq2h0wdgkgm6uozt1y9.png',
+        alt: "An image of the app screens",
+        name:"Todo App",
+        description : "An app that can create, edit and delete todos",
+        category: "Web Development, HTML, CSS, JavaScript" ,
+        project:"https://www.jayma.design/redesigning-rental-pickups-uber",
+        repo:"https://github.com/blessdamy/Blessing-Todo-Assignment"    ,
+        projectlink: "https://damy-todo.netlify.app"
+    },
+    {
+        id: 3,
+        image:'https://res.cloudinary.com/eiroro/image/upload/v1673534310/team-intel-portfolio/e1rxdfpc1sdadwojfjwk.png',
+        alt: "An image of the app screens",
+        name:"Review App",
+        description : "An app that can create, edit and delete todos",
+        category: "Web Development, HTML, TailwindCSS, ReactJS" ,
+        project:"https://www.jayma.design/redesigning-rental-pickups-uber",
+        repo:"https://github.com/blessdamy/Team-Intel-Footer"    ,
+        projectlink: "https://team-intel.netlify.app"
+    },
+];
+
+
+export default projects
+
+
+
+ 
+
