a6ba65d0c545a340d54c4aa84120943f7faf86d2
e-iroro <eruiroro18@gmail.com>
2023-01-14 14:44:06 +0100

    resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import Test from './components/Test';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import ProjectCard from './components/ProjectCard';
+import About from './components/About';
+import Work from "./components/Work"
+import projects from './project';
+
+
+function createProjectCard(project) {
+  return (
+    <ProjectCard
+      key = {project.id}
+      alt = {project.alt}
+      image = {project.image}
+      name = {project.name}
+      description = {project.description}
+      category = {project.category}
+      project = {project.project}
+      projectLink={project.projectlink}
+      repoLink={project.repo}
+     />
+  )
+}
+
+
 
 function App() {
   return (
-    <div className=" w-4/6 mx-auto ">
-        <Test />
+    <div className=" w-3/4 mx-auto ">
+        <Navbar />
+        <Hero />
+        {projects.map(createProjectCard)}
+        <Work/>
+        <About/>
     </div>
   );
 }
